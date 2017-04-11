43341108b3c21c43468aabf1ed802a9e4120fa16
Brian Ellis <be4833@login1.stampede.tacc.utexas.edu>
2017-04-11 15:41:34 -0500

    launch_mcl does not re-do work

--- scripts/launch_mcl.py
+++ scripts/launch_mcl.py
@@ -4,30 +4,35 @@ import os
 from config import *
 
 def mclOnSpec(sp):
+	print 'mcl on',sp
 	#os.system('mv '+PATH_TO_OUTPUT + sp + '/input.txt '+ PATH_TO_OUTPUT + sp + '/input_' + sp + '.txt' )
 	os.system(MCL_PATH+' ' + PATH_TO_OUTPUT + sp + '/input_' + sp + '.txt --abc -I 1.2')
 	os.system('mv  out.input_' + sp + '.txt.I12 ' + PATH_TO_OUTPUT + sp + '/')
 
 if __name__ == '__main__':
-	species = getSpecies()
+	species = giveMulti(getSelectedSpecies())
 	complete = []
-	for sp in giveMulti(species):
+	#makes a list of stuff we are done with
+	for sp in species:
 		try:
-			k=open(PATH_TO_OUTPUT + sp + '/out.input_'+sp+'.txt','r')
+			k=open(PATH_TO_OUTPUT + sp + '/out.input_'+sp+'.txt.I12','r')
 			complete.append(sp)
+			k.close()
 		except:
 			pass
 
 	todoSpec = []
-	for sp in giveMulti(species):
+	#makes the list of stuff left to do
+	for sp in species:
 		if sp in complete:
 			continue
 		try:
 			k=open(PATH_TO_OUTPUT + sp + '/input_'+sp+'.txt','r')
 			todoSpec.append(sp)
+			k.close()
 		except:
 			pass
+	#now lets get cracking
 	species = todoSpec
-	print len(species)
 	p = Pool(MAX_THREADS)
 	p.map(mclOnSpec,species)
--- scripts/test.py
+++ scripts/test.py
@@ -1,4 +1,28 @@
 from config import *
 import os
 
-print MCL_PATH
+
+species = giveMulti(getSelectedSpecies())
+print species
+complete = []
+for sp in species:
+	try:
+		k=open(PATH_TO_OUTPUT + sp + '/out.input_'+sp+'.txt.I12','r')
+		complete.append(sp)
+		k.close()
+	except:
+		pass
+
+todoSpec = []
+for sp in species:
+	if sp in complete:
+		continue
+	try:
+		k=open(PATH_TO_OUTPUT + sp + '/input_'+sp+'.txt','r')
+		todoSpec.append(sp)
+		k.close()
+	except:
+		pass
+species = todoSpec
+
+print species
