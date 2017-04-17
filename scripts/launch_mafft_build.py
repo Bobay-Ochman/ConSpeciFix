14a29eed395f0e281911741e20309fa0fab57921
Brian Ellis <brian.ellis@austin.rr.com>
2017-04-17 15:09:49 -0500

    build correctly skips completed work

--- scripts/launch_mafft_build.py
+++ scripts/launch_mafft_build.py
@@ -1,21 +1,20 @@
 from config import *
 
 species = getSelectedSpecies()
-species = ['Achromobacter_xylosoxidans']
+#species = ['Acetobacter_pasteurianus']
 out = open('todo/mafft.txt','w')
 out.seek(0)
 for sp in species:
 	files = os.listdir(PATH_TO_OUTPUT + sp + '/align/')
 	done = []
 	files = files[::-1]
+	#print files
 	for fichier in files:
+		#print done
 		if str(fichier).endswith('.fa.align'):
 			#We've already done it, we'll let them know
-			#done.append(str(fichier).strip('.align'))
 			continue
-		if str(fichier).strip('a') in done:
-			#skip it, we've done it already
-			continue
-		out.write(sp + '\t' + fichier + '\n');
+		if fichier+'.align' not in files:
+			out.write(sp + '\t' + fichier + '\n');
 out.truncate()
 out.close()
