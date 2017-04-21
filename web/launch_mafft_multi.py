c4d4019e4a5c7ca3abfa5573785d38d57f4318b8
Brian Ellis <brian.e2014@gmail.com>
2017-04-21 16:00:47 -0500

    duped all files for web server

---
+++ web/launch_mafft_multi.py
@@ -0,0 +1,21 @@
+from config import *
+import os
+from multiprocessing import Pool
+
+def launchMafft(args):
+
+	sp = args.split('\t')[0]
+	fichier= args.split('\t')[1].strip('\n')
+	print sp, fichier
+	os.system(MAFFT_PATH+'  ' + PATH_TO_OUTPUT + sp + '/align/' + fichier + '  >   '  + PATH_TO_OUTPUT + sp + '/align/' + fichier + '.align > /dev/null 2>&1')
+
+if __name__ == '__main__':
+
+	species = getSelectedSpecies()
+	p = Pool(MAX_THREADS)
+	f = open('todo/mafft.txt','r')
+	args = []
+	for l in f:
+		args.append(l)
+	args = giveMulti(args)
+	p.map(launchMafft,args)
