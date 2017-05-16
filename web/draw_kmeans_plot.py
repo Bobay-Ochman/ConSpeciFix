76b8cfab65af0c39919e800186f6bac22db3ab72
Brian <brian.e2014@gmail.com>
2017-05-16 16:26:26 -0500

    creates new plot with newlines

--- web/draw_kmeans_plot.py
+++ web/draw_kmeans_plot.py
@@ -11,7 +11,7 @@ for sp in species:
 	h.write("pdf('"+PATH_TO_UPLOAD+"dessin_" + sp + ".pdf')\n")
 	h.write("v=1:length(tab$V1)\n")
 	h.write("plot(tab$V5,pch=16,type='h',cex=0.7,ylim=c(0,100),ylab='%',xlab=('Strains'),col='grey')\n")
-	h.write("points(finpoint$V5,col='red',pch=16)")
+	h.write("points(finpoint$V5,col='red',pch=16)\n")
 	h.write("dev.off()\n")
 	h.close()
 	os.system("Rscript  "+PATH_TO_UPLOAD+"draw.R ")
