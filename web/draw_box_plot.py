9db1d3fdb2d434803f9bfc1c6b25ef7b9d0348ab
Brian <brian.e2014@gmail.com>
2017-05-16 18:02:20 -0500

    with sigfigs

--- web/draw_box_plot.py
+++ web/draw_box_plot.py
@@ -11,9 +11,9 @@ h.write("tabwout = tab$V2[which(tab$V3=='without')]\n")
 h.write("tabw = tab$V2[which(tab$V3=='with')]\n")
 h.write("W = wilcox.test(tabw,tabwout)[1]$statistic\n")
 h.write("P = wilcox.test(tabw,tabwout)[3]$p.value\n")
-h.write("t = t.test(v,w)[1]$statistic\n")
-h.write("Pt =  t.test(v,w)[3]$p.value\n")
-h.write("longlab= paste('Wilcoxon test: W=',W,'P=',P,'/n','t test: t=',t,'P=',Pt,sep=' ')\n")
+h.write("t = t.test(tabw,tabwout)[1]$statistic\n")
+h.write("Pt =  t.test(tabw,tabwout)[3]$p.value\n")
+h.write("longlab= paste('Wilcoxon test: W=',signif(W, digits=3),'P=',signif(P, digits=3),'/n','t test: t=',signif(t, digits=3),'P=',signif(Pt, digits=3),sep=' ')\n")
 
 h.write("pdf('"+PATH_TO_UPLOAD+"boxPlot.pdf')\n")
 
