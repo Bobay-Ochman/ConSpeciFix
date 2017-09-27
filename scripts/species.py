3da904d6ec0b8f21f4bf145c694847f8b205ee02
Brian <brian.e2014@gmail.com>
2017-09-27 11:34:49 -0500

    some git issues resolved

--- scripts/species.py
+++ scripts/species.py
@@ -32,12 +32,10 @@ h=open(PATH_TO_SPECIES_TXT,'w')
 NB=0
 for sp in species:
 	#We need at least 15 to have statistically relevant datasets.
-	if dico[sp] >= 15 and dico[sp]< 20:
+	if dico[sp] >= 15:
 		NB+=1
 		#print sp,' ',dico[sp]
 		h.write(sp + '\t' + str(dico[sp]) + '\n')
-	if dico[sp] > 19 and dico[sp]<30:
-		print 'rm -r ', sp
 h.truncate()
 h.close()
 print NB
