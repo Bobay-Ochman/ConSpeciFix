9d601d8f86dc52a50b5ab0bcdf2ba481ba9e09d6
Brian <brian.e2014@gmail.com>
2017-10-06 15:31:05 -0500

    now with ability to sample for clades

--- folderCompare/calcHM_import.py
+++ folderCompare/calcHM_import.py
@@ -1,116 +1,47 @@
-
-#path ="/Users/ochmanlab/Desktop/drew/"
-
 import os
-import sys
-
 from config import *
-
-
-SP = getSelectedSpecies()
-
-SP = 'Acetobacter_pasteurianus'
-
-# Load distances
-strains=[]
-f=open(PATH_TO_OUTPUT + SP + '/sample.txt','r')
-for l in f:
-	a=l.strip("\n").split("\t")
-	strains.append(a[0])
-f.close()
-
-
-dist={}
-f=open(PATH_TO_OUTPUT + SP + '/RAxML_distances.dist',"r")
-for l in f:
-	a=l.strip("\n").split("\t")
-	st1,st2 = a[0].strip(" ").split(" ")[0], a[0].strip(" ").split(" ")[1]
-	if dist.has_key(st1):
-		pass
-	else:
-		dist[st1] = {}
-	if dist.has_key(st2):
-		pass
-	else:
-		dist[st2] = {}
-	dist[st1][st2] = float(a[1])
-	dist[st2][st1] = float(a[1])
-f.close()
+from multiprocessing import Pool
+import multiprocessing
+import traceback
 
 
 
 
-tmp={}
-f=open(PATH_TO_OUTPUT + SP + '/concat85.fa',"r")
-for l in f:
-	if l[0] == '>':
-		nb=0
-		tag=0
-		sp = l.strip('>').strip('\n') 
-		tmp[sp] = []
-	else:
-		nb += len(l.strip('\n'))
-		tmp[sp].append(l.strip('\n'))
-f.close()
-
-
-seq = {}
-for sp in strains:
-	seq[sp] = ''.join(tmp[sp])
-
-
-#strains.remove('Dsim')
-
-memo_subset={}
-tmp = os.listdir(PATH_TO_OUTPUT + SP + '/')
-for file in tmp:
-	if file.startswith("rm"):
-		f=open( PATH_TO_OUTPUT + SP + '/' + file,"r")
-		for l in f:
-			a=l.strip("\n").split("\t")
-			if len(a) == 5:									
-				memo_subset[a[0]] = a[1:]
-		f.close()
+def calcHMLoads(args,seq,dist):
+	p = Pool(MAX_THREADS)
+	f = open(PATH_TO_TODO + 'calcHM.txt','r')
+	tupleArgs = []
+	for l in args:
+		tupleArgs.append((l,seq,dist))
+	return p.map(wrapper,reversed(tupleArgs))
 
 
 
-f_subset=open(PATH_TO_OUTPUT + SP + '/rm1.txt',"w")
-for subset in memo_subset:
-	f_subset.write(subset + "\t" + '\t'.join(memo_subset[subset])  + "\n")
 
-f_subset.close()
-
-
-
-tmp=[]
-f=open(PATH_TO_OUTPUT + SP + '/families_'+SP+'.txt','r')
-for l in f:
-	a=l.strip('\n').split('\t')
-	if memo_subset.has_key(a[1]):
-		pass
-	else:
-		tmp.append(a[1])
+def wrapper(tupleArg):
+	try:
+		return calcHM(tupleArg[0],tupleArg[1],tupleArg[2])
+	except Exception as e:
+		print e
+		traceback.print_exc()
+		h = open (PATH_TO_TODO+'error.txt','a')
 
 
-import random
 
-subsets=[]
-while len(tmp) > 0:
-	truc = random.choice(tmp)
-	subsets.append(truc)
-	tmp.remove(truc)
+def calcHM(args,seq,dist):
 
+	truc = args.strip('\n')
+		
+	alpha=['A','C','G','T']
+	strains = truc.split('&&&')
 
-print 'GO'
+	LONGUEUR = len(seq[strains[0]])
+	for sp in strains:
+		LONGUEUR=min(LONGUEUR,len(seq[sp]))
 
-alpha=['A','C','G','T']
-
-LONGUEUR=len(seq[sp])
-if LONGUEUR > 1000000:
-	LONGUEUR = 1000000
+	if LONGUEUR > 1000000:
+		LONGUEUR = 1000000
 	
-for truc in subsets:
-	strains = truc.split('-')
 	bip=[]
 	singleton,more=0,0
 	i = 0
@@ -323,13 +254,6 @@ for truc in subsets:
 		i+=1
 	try:
 		rm = float(r)/m
-	except ZeroDivisionError:
-		rm = 'NA'
-	print  len(strains),' r/m= ', rm      #,' r= ',r,' m= ',m	, '   Bips:  r= ',bip.count('r'),'  m= ',bip.count('m'),' |  for ',singleton,' singleton'
-	h=open(PATH_TO_OUTPUT + SP + '/rm1.txt',"a")
-	h.write(truc + '\t' + str(r) + '\t' + str(m) + '\t' + str(rm) + '\t' + str(len(bip)) + '\n'   )
-	h.close()
-
-
-
-
+		return rm
+	except:
+		return -1
