b2faebdb7126348c0d39ab71a02c575b4fca9108
Brian <brian.e2014@gmail.com>
2018-10-11 11:23:53 -0500

    more debug

--- database/child_launcher.py
+++ database/child_launcher.py
@@ -4,4 +4,10 @@ print "hello from launcher"
 print os.getcwd()
 print os.listdir(os.getcwd())
 
+from config import *
+try:
+	print PATH_TO_OUTPUT
+except:
+	print 'whelp this will be not fun'
+
 os.system('python child_runner.py')
